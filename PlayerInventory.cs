85f998460ac9ab64204e78f2877c8242d420fdbe
HakuRDev <121049280+HakuRDev@users.noreply.github.com>
2023-01-25 16:16:18 +0300

    Add files via upload
    https://youtu.be/JhcYvkhWh5w

---
+++ PlayerInventory.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace SG
+{
+
+
+   public class PlayerInventory : MonoBehaviour
+  {
+    WeaponSlotManager weaponSlotManager;
+
+    public WeaponItem rightWeapon;
+    public WeaponItem leftWeapon;
+
+    private void Awake()
+    {
+        weaponSlotManager = GetComponentInChildren<WeaponSlotManager>();
+    }
+
+    private void start()
+    {
+        weaponSlotManager.LoadWeaponOnSlot(rightWeapon, false);
+        weaponSlotManager.LoadWeaponOnSlot(leftWeapon, true);
+    }
+
+  }
+
+
+
+}
